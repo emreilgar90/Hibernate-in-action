ad33671167f6807f067333e3d5f3bea0f0b3e163
Emre ILGAR <emreilgar90@gmail.com>
2024-02-14 16:32:56 +0300

    Initial commit

---
+++ src/main/java/com/emreilgar/hibernateinaction/HibernateInActionApplication.java
@@ -0,0 +1,13 @@
+package com.emreilgar.hibernateinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emreilgar/hibernateinaction/HibernateInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreilgar.hibernateinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
